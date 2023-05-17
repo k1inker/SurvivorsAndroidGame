ded65852d174b304e1ca611af6fbcb394f609426
k1inker <penzenskiy.evgeniy@gmail.com>
2023-05-17 09:58:56 +0200

    change logic movement from kinematic to dynamic, add new 2d arts

--- Assets/Scripts/Character/CharacterLocomotionManager.cs
+++ Assets/Scripts/Character/CharacterLocomotionManager.cs
@@ -14,9 +14,7 @@ public abstract class CharacterLocomotionManager : MonoBehaviour
     protected List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
     protected CharacterManager _character;
 
-    private Vector2 directionPush;
-    private float knockbackForce;
-    private bool hasUnchangingPath = false;
+    private bool isStunned = false;
     protected virtual void Awake()
     {
         _capsuleCollider = GetComponentInChildren<CapsuleCollider2D>();
@@ -25,33 +23,16 @@ public abstract class CharacterLocomotionManager : MonoBehaviour
     }
     public virtual void HandelMovment(Vector2 moveVector)
     {
+        if (isStunned)
+            return;
+
         if (moveVector == Vector2.zero)
         {
+            rb.velocity = Vector2.zero;
             //_character.characterAnimatorManager.SetAnimatorMove(false);
             return;
         }
-        else
-        {
-            // logic pushingBack
-            float speed = moveSpeed;
-
-            if (hasUnchangingPath)
-            {
-                speed = knockbackForce;
-                moveVector = directionPush;
-            }
-
-            bool isMove = TryMove(moveVector,speed);
-            if (!isMove)
-            {
-                isMove = TryMove(new Vector2(moveVector.x, 0), speed);
-            }
-            if (!isMove)
-            {
-                isMove = TryMove(new Vector2(0, moveVector.y), speed);
-            }
-            //_character.characterAnimatorManager.SetAnimatorMove(isMove);
-        }
+        rb.velocity = moveVector * moveSpeed * Time.deltaTime;
 
         if (moveVector.x < 0)
         {
@@ -62,38 +43,15 @@ public abstract class CharacterLocomotionManager : MonoBehaviour
             _character.characterRenderer.flipX = false;
         }
     }
-    // locomotion for kinematics object
-    protected virtual bool TryMove(Vector2 direction, float speed)
+    public virtual void KnockBack(Vector2 direction, float knockbackForce)
     {
-        if (direction == Vector2.zero)
-        {
-            return false;
-        }
-        int count = _capsuleCollider.Cast(direction,
-                        contactFilter,
-                        castCollisions,
-                        moveSpeed * Time.deltaTime + collisionOffset);
-
-        if (count == 0)
-        {
-            rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        rb.AddForce(direction * knockbackForce, ForceMode2D.Impulse);
+        StartCoroutine(StunTime(.15f));
     }
-    public void KnockBack(Vector2 direction, float knockbackForce)
+    private IEnumerator StunTime(float time)
     {
-        hasUnchangingPath = true;
-        directionPush = direction;
-        this.knockbackForce = knockbackForce;
-        StartCoroutine(KnockBackShock());
-    }
-    private IEnumerator KnockBackShock()
-    {
-        yield return new WaitForSeconds(.15f);
-        hasUnchangingPath = false;
+        isStunned = true;
+        yield return new WaitForSeconds(time);
+        isStunned = false;
     }
 }
